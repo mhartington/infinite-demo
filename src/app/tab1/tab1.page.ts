19d435d3a23148f4100ea87578d30aad468806d5
Mike Hartington <mikehartington@gmail.com>
2022-12-07 16:16:30 -0500

    add demo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  onInfinite(event: any) {
+    console.log('onInfinite fired');
+    setTimeout(() => {
+      this.items = [...this.items, ...Array(50).keys()];
+      event.target.complete();
+      console.log('onInfinite complete');
+    }, 2000);
+  }
+  items = Array.from(Array(50).keys());
 
   constructor() {}
-
 }
